703e98a6e4d7e347490d3200ac0793f367b2a0a4
alexa01010 <alexandrasousa482@gmail.com>
2024-09-14 20:42:50 -0300

    atualixado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+///função para o slide mexer sozinho
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    NextImage();
+}, 3000)
+
+function NextImage(){
+    count++;
+    if(count>4){
+    count = 1;
+    }
+    document.getElementById("radio"+count).checked = true;
+}
+
+const navbar = document.querySelector(".navbar");
+const menuButton = document.querySelector(".menu-button");
+
+menuButton.addEventListener("click", () => {
+    navbar.classList.toggle("show-menu");
+});
+
+
+
+
 
