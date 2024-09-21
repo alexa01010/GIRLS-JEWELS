45e62848b2d7160ad5c02ef0ffd6971c4c5eb260
alexa01010 <alexandrasousa482@gmail.com>
2024-09-21 19:42:48 -0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
+const navbar = document.querySelector(".navbar");
+const menuButton = document.querySelector(".menu-button");
+
+menuButton.addEventListener("click", () => {
+    navbar.classList.toggle("show-menu");
+});
+
 ///função para o slide mexer sozinho
+
 let count = 1;
 document.getElementById("radio1").checked = true;
 
 setInterval( function(){
     NextImage();
-}, 3000)
+}, 5000)
 
 function NextImage(){
     count++;
@@ -14,22 +22,11 @@ function NextImage(){
     document.getElementById("radio"+count).checked = true;
 }
 
-const navbar = document.querySelector(".navbar");
-const menuButton = document.querySelector(".menu-button");
-
-menuButton.addEventListener("click", () => {
-    navbar.classList.toggle("show-menu");
-});
 
 
 
 
 
 
-const navbar = document.querySelector(".navbar");
-const menuButton = document.querySelector(".menu-button");
 
 
-menuButton.addEventListener("click", () => {
-    navbar.classList.toggle("show-menu");
-});
